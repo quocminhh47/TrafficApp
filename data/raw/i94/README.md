3cd9205ed760aa476de63f124715fdd63693dfed
quocminhh47 <quocminhh47@gmail.com>
2025-11-23 11:26:00 +0700

    clean up

--- data/raw/i94/README.md
+++
@@ -1,4 +0,0 @@
-# Regression
-## Metro Interstate Traffic Volume
-
-This data set contains Hourly weather features and holidays included for impacts on traffic volume..The main objective is to clean the data and fit it with a regression model to predict the traffic volume.
--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd, glob
-files = glob.glob("data/processed_ds/Minneapolis-StPaul/I-94 Corridor/part-i94-d4b26e3f5d9b439a9df55f877943be45.parquet", recursive=True)
-# df = pd.read_parquet("data/processed_ds/Minneapolis-StPaul/I-94 Corridor/part-i94-d4b26e3f5d9b439a9df55f877943be45.parquet")
-for f in files:
-    df = pd.read_parquet(f)
-    if "I-94-WB" in df["RouteId"].astype(str).unique():
-        print(f)
-        g = df[df["RouteId"]=="I-94-WB"]
-        print("min:", g["DateTime"].min(), "max:", g["DateTime"].max(), "rows:", len(g))
